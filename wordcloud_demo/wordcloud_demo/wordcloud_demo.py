3285bd89c32081c54f850fd870784bc91de04eb2
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-09-02 18:12:07 +0900

    Demo 페이지 갱신

--- wordcloud_demo/wordcloud_demo/wordcloud_demo.py
+++ wordcloud_demo/wordcloud_demo/wordcloud_demo.py
@@ -13,7 +13,20 @@ filename = f"{config.app_name}/{config.app_name}.py"
 class State(rx.State):
     """The app state."""
 
-    pass
+    width: int = 600
+
+    height: int = 300
+
+    max_words: int = 50
+
+    def set_width(self, width: int):
+        self.width = width
+
+    def set_height(self, height: int):
+        self.height = height
+
+    def set_max_words(self, max_words: int):
+        self.max_words = max_words
 
 
 def index() -> rx.Component:
@@ -26,15 +39,47 @@ def index() -> rx.Component:
                 rx.code(filename),
                 font_size="2em",
             ),
-            wordcloud(
-                words=words,
-                options={
-                    "rotations": 2,
-                    "rotationAngles": [-90, 0],
-                    "fontFamily": "impact",
-                },
-                size=(5, 5),
-                min_size=(400, 200),
+            rx.hstack(
+                wordcloud(
+                    words=words,
+                    max_words=State.max_words,
+                    options={
+                        "rotations": 2,
+                        "rotationAngles": [-90, 0],
+                        "fontFamily": "impact",
+                    },
+                    size=(State.width, State.height),
+                ),
+            ),
+            rx.hstack(
+                rx.text(f"height({State.height})"),
+                rx.slider(
+                    min=200,
+                    max=400,
+                    default_value=State.height,
+                    on_value_commit=State.set_height,
+                ),
+                width="100%",
+            ),
+            rx.hstack(
+                rx.text(f"width({State.width})"),
+                rx.slider(
+                    min=400,
+                    max=800,
+                    default_value=State.width,
+                    on_value_commit=State.set_width,
+                ),
+                width="100%",
+            ),
+            rx.hstack(
+                rx.text(f"max_words({State.max_words})"),
+                rx.slider(
+                    min=10,
+                    max=100,
+                    default_value=State.max_words,
+                    on_value_commit=State.set_max_words,
+                ),
+                width="100%",
             ),
             align="center",
             spacing="7",
